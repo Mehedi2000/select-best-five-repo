9fa82916304e32c56a8fd2e5a9935bbb0095ca5a
Mehedi2000 <mh4054528@gmail.com>
2022-08-23 13:18:48 +0600

    added selected section

---
+++ js/players.js
@@ -0,0 +1,42 @@
+const plyerNameArry = [];
+
+function display(playerName) {
+    const tableBody = document.getElementById("player-products");
+    tableBody.innerHTML = "";
+
+    for (let i = 0; i < playerName.length; i++) {
+        const name = plyerNameArry[i].playerName;
+
+        const tr = document.createElement("tr");
+
+        tr.innerHTML = `
+<th>${i + 1}</th>
+<td>${name}</td>
+        `;
+
+        tableBody.appendChild(tr);
+    }
+}
+
+function addPlayer(element) {
+
+    const playerName = element.parentNode.children[0].innerText;
+
+    const playerNameObject = {
+        playerName: playerName
+    }
+
+    if (plyerNameArry.length < 5) {
+        plyerNameArry.push(playerNameObject);
+
+    }
+    else {
+        alert('Only five player is allowed')
+    }
+
+
+    document.getElementById("total-player-added").innerText = plyerNameArry.length;
+
+    display(plyerNameArry);
+}
+
